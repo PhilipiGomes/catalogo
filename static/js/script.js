807ca3cf50f9711663e25a356d2e610dea45aa37
PhilipiGomes <philipi.rodrigues342@gmail.com>
2025-08-04 08:03:25 -0300

    TODO: Arrumar o carrossel, aprimorar as cores, mais JS, Pegar imagens melhores.

--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,37 @@
 // botao de voltar ao topo
 
 document.addEventListener("scroll", function () {
-    const btn = document.getElementById("btntopo");
+  const btn = document.getElementById("btntopo");
 
-    // mostrar ou esconder o botão de rolagem de página
-    window.addEventListener("scroll", function () {
+  // mostrar ou esconder o botão de rolagem de página
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  });
 
-        if (window.scrollY > 300) {
-            btn.style.display = "block";
-        } else {
-            btn.style.display = "none";
-        }
+  // rolar a pagina ate o topo, ao clicar no botao
+  btn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
-
-    // rolar a pagina ate o topo, ao clicar no botao
-    btn.addEventListener("click", function () {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    });
-
+  });
 });
 
 function abrirModal(id) {
-    document.getElementById(id).style.display = "block";
+  document.getElementById(id).style.display = "block";
 }
 
 function fecharModal(id) {
-    document.getElementById(id).style.display = "none";
+  document.getElementById(id).style.display = "none";
 }
 
 window.addEventListener("click", function (event) {
-    const login = document.getElementById("loginModal");
-    const cadastro = document.getElementById("cadastroModal");
-    if (event.target === login) login.style.display = "none";
-    if (event.target === cadastro) cadastro.style.display = "none";
-    
+  const login = document.getElementById("loginModal");
+  const cadastro = document.getElementById("cadastroModal");
+  if (event.target === login) login.style.display = "none";
+  if (event.target === cadastro) cadastro.style.display = "none";
 });
